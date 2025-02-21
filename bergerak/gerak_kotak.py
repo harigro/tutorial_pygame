3fe623192bba46dc2e735696496f28cb02b89254
harigro <trierbank@gmail.com>
2025-02-21 14:33:57 +0700

    memperbaiki move_ip dan fps

--- bergerak/gerak_kotak.py
+++ bergerak/gerak_kotak.py
@@ -10,38 +10,48 @@ BACKGROUND_COLOR = (30, 30, 30)
 # Warna dan ukuran kotak
 BOX_COLOR = (0, 255, 0)
 BOX_SIZE = 50
-x, y = (WIDTH - BOX_SIZE) // 2, (HEIGHT - BOX_SIZE) // 2
 VELOCITY = 5
+
+# FPS
+FPS = 60
+clock = pygame.time.Clock()
+
+# rect
+kotak = pygame.Rect((WIDTH - BOX_SIZE) // 2, (HEIGHT - BOX_SIZE) // 2, BOX_SIZE, BOX_SIZE)
 
 # Buat layar game
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gerakkan Kotak")
 
-# Loop utama
-running = True
-while running:
-    pygame.time.delay(30)  # Tambahkan sedikit jeda untuk mengontrol kecepatan
+def main():
+    # Loop utama
+    running = True
+    while running:
 
-    # Cek event
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # Cek event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Ambil input dari keyboard
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x > 0:
-        x -= VELOCITY
-    if keys[pygame.K_RIGHT] and x < WIDTH - BOX_SIZE:
-        x += VELOCITY
-    if keys[pygame.K_UP] and y > 0:
-        y -= VELOCITY
-    if keys[pygame.K_DOWN] and y < HEIGHT - BOX_SIZE:
-        y += VELOCITY
+        # Ambil input dari keyboard
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and kotak.x > 0:
+            kotak.move_ip(-VELOCITY, 0)
+        if keys[pygame.K_RIGHT] and kotak.x < WIDTH - BOX_SIZE:
+            kotak.move_ip(VELOCITY, 0)
+        if keys[pygame.K_UP] and kotak.y > 0:
+            kotak.move_ip(0, -VELOCITY)
+        if keys[pygame.K_DOWN] and kotak.y < HEIGHT - BOX_SIZE:
+            kotak.move_ip(0, VELOCITY)
 
-    # Gambar ulang layar
-    screen.fill(BACKGROUND_COLOR)
-    pygame.draw.rect(screen, BOX_COLOR, (x, y, BOX_SIZE, BOX_SIZE))
-    pygame.display.update()
+        # Gambar ulang layar
+        screen.fill(BACKGROUND_COLOR)
+        pygame.draw.rect(screen, BOX_COLOR, kotak)
+        clock.tick(FPS)
+        pygame.display.update()
 
-# Keluar dari game
-pygame.quit()
+    # Keluar dari game
+    pygame.quit()
+
+if __name__=="__main__":
+    main()
